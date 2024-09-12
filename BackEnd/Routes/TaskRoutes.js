f8f64974d51ccf44f2f8871d58ebe1c8c1256dda
Raaazamit1701 <amitraaaz0@gmail.com>
2024-09-13 00:33:44 +0530

    addeding

--- BackEnd/Routes/TaskRoutes.js
+++ BackEnd/Routes/TaskRoutes.js
@@ -13,12 +13,12 @@ taskRoutes.get("/getTask", async (req, res) => {
 });
 
 taskRoutes.post("/postTask", async (req, res) => {
-  const { _id } = req.user;
+  // const { _id } = req.user;
   const newTask = req.body;
   console.log(_id);
 
   await dataModel
-    .findByIdAndUpdate({ _id: _id }, { $push: { tasks: newTask } })
+    .create({ _id: newTask.id}, { $push: { tasks: newTask } })
     .catch((err) => {
       console.log(err);
     });
